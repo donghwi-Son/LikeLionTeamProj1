bb2f73738a60d3513bf1a0376212fd65a6621579
donghwiSon <ehdgnlgk@gmail.com>
2025-03-21 11:09:53 +0900

    1차커밋테스트

---
+++ ShootingGameGroup3/Assets/Scripts/sdh_ShieldEnemy.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class sdh_ShieldEnemy : MonoBehaviour
+{
+    public float HP = 10;
+    Transform pt;   //플레이어 transform
+    float speed = 1f;
+    float turnDelay = 0.1f;
+    float nextCheckTime = 0;
+    private void Start()
+    {
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        if (playerObject != null)
+        {
+            pt = playerObject.transform;
+        }
+    }
+    private void Update()
+    {
+
+        Vector3 direction = (pt.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+        if (Time.time >= nextCheckTime)
+        {
+            CheckFlipX();
+            nextCheckTime = Time.time + turnDelay; // 다음 체크 시간 갱신
+        }
+
+    }
+
+    void CheckFlipX()
+    {
+        if (transform.position.x < pt.position.x) // 적이 왼쪽
+        {
+            if (Random.value > 0.5f) // 50% 확률로 행동
+            {
+                Invoke("TurnLeft", 0.5f);
+            }
+        }
+        else
+        {
+            if (Random.value > 0.5f) // 50% 확률로 행동
+            {
+                Invoke("TurnRight", 0.5f);
+            }
+        }
+    }
+    
+    void TurnLeft()
+    {
+        Vector3 v = transform.rotation.eulerAngles;
+        v.y = 180;
+        transform.rotation = Quaternion.Euler(v);
+    }
+
+    void TurnRight()
+    {
+        Vector3 v = transform.rotation.eulerAngles;
+        v.y = 0;
+        transform.rotation = Quaternion.Euler(v);
+    }
+}
