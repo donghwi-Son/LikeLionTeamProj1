c3573a3aff0a7ecf1e9361b22b5f585cf9ef689b
donghwiSon <ehdgnlgk@gmail.com>
2025-03-21 15:32:36 +0900

    적 추가 / 스크립트 수정
    전반적인 움직임을 rigidbody로 변경
    플레이어 대쉬 추가

--- ShootingGameGroup3/Assets/Scripts/sdh/sdh_Player.cs
+++ ShootingGameGroup3/Assets/Scripts/sdh/sdh_Player.cs
@@ -1,28 +1,38 @@
+using System.Collections;
 using UnityEngine;
 
 public class sdh_Player : MonoBehaviour
 {
-    float speed = 5f;
+    
+    SpriteRenderer psr; 
+    SpriteRenderer gunsr;
+    Rigidbody2D rb;
+    float speed = 3f;
     public Transform gunT;
-    private SpriteRenderer psr; 
-    private SpriteRenderer gunsr;
     public Transform fp;
+    bool isRolling = false;
+    bool isDashCool = false;
 
 
     void Start()
     {
-        // SpriteRenderer 컴포넌트 가져오기
+        rb = GetComponent<Rigidbody2D>();
         psr = GetComponent<SpriteRenderer>();
         gunsr = gunT.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        Move();
-        FlipPlayerAndGun();
+        if (!isRolling)
+        {
+            Move();
+            FlipPlayer();
+        }
+        FlipGun();
+        Roll();
     }
 
-    private void FlipPlayerAndGun()
+    private void FlipPlayer()
     {
         // 마우스 위치 계산
         Vector3 mousePosition = Input.mousePosition;
@@ -33,14 +43,28 @@ public class sdh_Player : MonoBehaviour
         {
             // 마우스가 왼쪽일 때
             psr.flipX = true;
-            gunsr.flipY = true; // 총도 수직 반전
-            fp.localPosition = new Vector3(fp.localPosition.x, -Mathf.Abs(fp.localPosition.y), fp.localPosition.z);
- 
         }
         else
         {
             // 마우스가 오른쪽일 때
             psr.flipX = false;
+        }
+    }
+
+    void FlipGun()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        // 마우스 위치에 따라 Flip 설정
+        if (mousePosition.x < transform.position.x)
+        {
+            gunsr.flipY = true; // 총 수직 반전
+            fp.localPosition = new Vector3(fp.localPosition.x, -Mathf.Abs(fp.localPosition.y), fp.localPosition.z);
+
+        }
+        else
+        {
             gunsr.flipY = false; // 총 원래 방향
             fp.localPosition = new Vector3(fp.localPosition.x, Mathf.Abs(fp.localPosition.y), fp.localPosition.z);
         }
@@ -57,8 +81,40 @@ public class sdh_Player : MonoBehaviour
             vec.Normalize();
         }
 
-        transform.Translate(vec * speed * Time.deltaTime);
+        rb.linearVelocity = vec * speed;
     }
 
+    void Roll()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !isRolling && !isDashCool)
+        {
+            StartCoroutine(StartRoll());
+        }
+    }
 
+    IEnumerator StartRoll()
+    {
+        isRolling = true;
+        isDashCool = true;
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector3 dir = new Vector3(moveX, moveY, 0).normalized;
+        if (dir == Vector3.zero)
+        {
+            Vector3 wP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 dirvec = wP - transform.position;
+            dir = new Vector3(dirvec.x, dirvec.y, 0).normalized;
+        }
+        rb.AddForce(dir * 10, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.2f);
+        rb.linearVelocity = Vector2.zero;
+        isRolling = false;
+        yield return new WaitForSeconds(2.8f);
+        isDashCool = false;
+    }
+
+    void GetHit()
+    {
+        //피격처리
+    }
 }
