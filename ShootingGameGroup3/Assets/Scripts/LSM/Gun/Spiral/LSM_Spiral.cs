ca2979569acd54c3e7054eb130577b51bb7e74e9
LEEYONGJAE113 <ionsi1103@gmail.com>
2025-03-26 15:24:44 +0900

    승민님거 병합 덜된거 다시 병합

---
+++ ShootingGameGroup3/Assets/Scripts/LSM/Gun/Spiral/LSM_Spiral.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LSM_Spiral : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 20f;
+    public float delay = 0.1f;
+
+    void Start() { }
+
+    void Update() { }
+}
